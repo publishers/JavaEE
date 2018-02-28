30f0dd2e5873254b2caf12873fa18bb81eca9e9e
Serhii Malykhin <sergeykvitoch@gmail.com>
2018-02-28 22:46:23 +0200

    modified sql

--- sql/shopcakes_statusOrder.sql
+++ sql/shopcakes_statusOrder.sql
@@ -1,6 +1,6 @@
-INSERT INTO shopcakes.statusOrder (status) VALUES ('ADOPTED');
-INSERT INTO shopcakes.statusOrder (status) VALUES ('CANCELLED');
-INSERT INTO shopcakes.statusOrder (status) VALUES ('COMPLETED');
-INSERT INTO shopcakes.statusOrder (status) VALUES ('CONFIRMED');
-INSERT INTO shopcakes.statusOrder (status) VALUES ('FORMED');
-INSERT INTO shopcakes.statusOrder (status) VALUES ('SENT');
+INSERT INTO shopcakes.statusOrder (id, status) VALUES (0, 'ADOPTED');
+INSERT INTO shopcakes.statusOrder (id, status) VALUES (1, 'CANCELLED');
+INSERT INTO shopcakes.statusOrder (id, status) VALUES (2, 'COMPLETED');
+INSERT INTO shopcakes.statusOrder (id, status) VALUES (3, 'CONFIRMED');
+INSERT INTO shopcakes.statusOrder (id, status) VALUES (4, 'FORMED');
+INSERT INTO shopcakes.statusOrder (id, status) VALUES (5, 'SENT');
